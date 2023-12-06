6640fe97094caae4ff5a41f599feb19c843ddba2
10969torrexx <pablitojrtorre@gmail.com>
2023-12-06 21:02:12 +0800

    Included products and categorized according to type, filter working

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+class ProductsController extends Controller
+{
+    public function showProductTypes() {
+      
+        $productTypes = [
+            'Shorts',
+            'Jackets',
+            'T-shirts',
+            'Dress',
+            'Pants'
+        ];
+
+        return $productTypes;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Products::all();
+        return $products;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/pages/products.blade.php
@@ -0,0 +1,48 @@
+@php
+    use App\Http\Controllers\ProductsController;
+    $productsController = new ProductsController;
+@endphp
+<!-- ======= Portfolio Section ======= -->
+<section id="products" class="portfolio">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+        <h2>Products</h2>
+        </div>
+
+        <ul id="portfolio-flters" class="d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+        <li data-filter="*" class="filter-active">All</li>
+        {{-- <li data-filter=".filter-app">App</li>
+        <li data-filter=".filter-card">Card</li>
+        <li data-filter=".filter-web">Web</li> --}}
+            @foreach ($productsController->showProductTypes() as $item)
+                <li data-filter=".filter-{{ $loop->iteration }}">{{ $item }}</li>
+            @endforeach
+        </ul>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+            @if (count($productsController->index()) > 0)
+                @foreach ($productsController->index() as $item)
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-{{ $item->category }}">
+                        <div class="shadow border-rounded portfolio-img"><img src="{{ $item->img }}" class="img-fluid" alt=""></div>
+                        <div class="portfolio-info">
+                        <h4>{{ ucwords($item->product_name) }}</h4>
+                        <p>App</p>
+                        <a href="{{ $item->img }}" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
+                        <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+                        </div>
+                    </div>
+                @endforeach
+            @else
+                <div class="col-lg-4 col-md-6 portfolio-item">
+                    <h1>No products to show</h1>
+                </div>
+            @endif
+
+
+        </div>
+
+    </div>
+</section>
+  <!-- End Portfolio Section -->
